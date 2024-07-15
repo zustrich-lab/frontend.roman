f24b1792b98741e8820ad4293c7137ef39e506ac
Назар Лимар <nazarlimar@MacBook-Pro-Nazar.local>
2024-07-15 22:31:39 +0200

    initial

--- src/Js/Leaderboard.js
+++ src/Js/Leaderboard.js
@@ -120,11 +120,11 @@ useEffect(() => {
               fontSize: '2vh',
               margin:'2vh',  
               color: 'white'}}>
-                
+
             </div>
               
               <div className='NameLb'>
-              <p> {userNickname} <br/><span id='LbColor'>{userRank ? `Rank: ${userRank}` : 'Loading...'}</span></p>
+              <p> {userNickname} <br/><span id='LbColor'>{'Loading...'}</span></p>
               <p id='LbColor'>{coins} OCTIES</p>
               </div>
             </div>
