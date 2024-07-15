09269fba9c16fc08fe6b6f6390444c8866627a8e
Назар Лимар <nazarlimar@MacBook-Pro-Nazar.local>
2024-07-15 22:17:50 +0200

    initial

--- src/Js/Leaderboard.js
+++ src/Js/Leaderboard.js
@@ -1,31 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import '../Css/Leaderboard.css';
 import axios from 'axios';
-
+import '../Css/Leaderboard.css';
 import logo from '../IMG/All_Logo/LBoard.png';
 
 const REACT_APP_BACKEND_URL = 'https://octiesback-production.up.railway.app';
 
-const Leaderboard = ({ LeaderboardAnim, userId, coins }) => {
+const Leaderboard = ({ LeaderboardAnim, userId }) => {
   const [leaderboard, setLeaderboard] = useState([]);
   const [userRank, setUserRank] = useState(null);
-  const [userCount, setUserCount] = useState(0);
-
-useEffect(() => {
-  const fetchUserCount = async () => {
-    try {
-      const response = await axios.get(`${REACT_APP_BACKEND_URL}/user-count`);
-      if (response.data.success) {
-        setUserCount(response.data.count);
-      }
-    } catch (error) {
-      console.error('Ошибка при получении количества пользователей:', error);
-    }
-  };
-
-  fetchUserCount();
-}, []);
-
+  const [nickname, setNickname] = useState('');  // Добавьте это состояние
 
   useEffect(() => {
     const fetchLeaderboard = async () => {
@@ -38,25 +21,33 @@ useEffect(() => {
         console.error('Ошибка при загрузке лидерборда:', error);
       }
     };
-  
-    const fetchUserRank = async () => {
+
+    const fetchUserRankAndNickname = async () => {
       try {
-        console.log(`Fetching rank for userId: ${userId}`); 
-        const response = await axios.get(`${REACT_APP_BACKEND_URL}/user-rank`, { params: { userId } });
-        if (response.data.success) {
-          console.log('User rank fetched successfully:', response.data.rank); 
-          setUserRank(response.data.rank);
+        console.log(`Fetching rank and nickname for userId: ${userId}`);  // Логирование userId
+        const rankResponse = await axios.get(`${REACT_APP_BACKEND_URL}/user-rank`, { params: { userId } });
+        const userDataResponse = await axios.get(`${REACT_APP_BACKEND_URL}/get-user-data`, { params: { userId } });
+        
+        if (rankResponse.data.success) {
+          console.log('User rank fetched successfully:', rankResponse.data.rank);  // Логирование успешного ответа
+          setUserRank(rankResponse.data.rank);
         } else {
-          console.error('Error in response data:', response.data.message);
+          console.error('Error in response data:', rankResponse.data.message);
+        }
+        
+        if (userDataResponse.data) {
+          setNickname(userDataResponse.data.nickname);  // Установите ник пользователя
+        } else {
+          console.error('Error in user data response:', userDataResponse.data.message);
         }
       } catch (error) {
-        console.error('Ошибка при загрузке позиции пользователя:', error);
+        console.error('Ошибка при загрузке данных пользователя:', error);
       }
     };
 
     fetchLeaderboard();
     if (userId) {
-      fetchUserRank();
+      fetchUserRankAndNickname();
     } else {
       console.error('userId не определен');
     }
@@ -83,90 +74,58 @@ useEffect(() => {
     }
     return color;
   };
-    return (
-      <div className={`Lb_Window ${LeaderboardAnim? 'fade-out' : ''}`}>
-        <div className='lb_Info'>
-          <p>Telegram Wall of Fame</p>
-        </div>
-        
-        <div className='Lb_Menu'>
 
+  return (
+    <div className={`Lb_Window ${LeaderboardAnim ? 'fade-out' : ''}`}>
+      <div className='lb_Info'>
+        <p>Telegram Wall of Fame</p>
+      </div>
+      
+      <div className='Lb_Menu'>
         <div className='LbBorder'>
-            <div className='Lb_Logo'>
-              <img src={logo} alt='logo'/>
-            </div>
-            <div className='Lb_Text'>
-              <p>🥇The 1st holder will get 400,000 OCTIES</p>
-              <p>🥈The 2nd holder will get 250,000 OCTIES</p>
-              <p>🥉The 3rd holder will get 100,000 OCTIES</p>
-            </div>
+          <div className='Lb_Logo'>
+             <img src={logo} alt='logo'/>
           </div>
-
-          <div className='Lb_inside'>
-            <div className='LbPhoto'>
-            <div className="RamdomImage" 
-
-            style={{
-              backgroundColor: getRandomColor(), 
-              borderRadius: '50%', 
-              aspectRatio: '1', 
-              height: '60%', 
-              display: 'flex', 
-              alignItems: 'center', 
-              justifyContent: 'center', 
-              fontSize: '2vh',
-              margin:'2vh',  
-              color: 'white'}}>
-
-            </div>
-              
-              <div className='NameLb'>
-                <p> Current User </p>
-                <p id='LbColor'>{coins} OCTIES</p>
-              </div>
-            </div>
-            <div className='LbPhoto'>
-              <p id="number">{userRank ? `#${userRank}` : '??'}</p>
-            </div>
+          <div className='Lb_Text'>
+            <p>🥇The 1st holder will get 400,000 OCTIES</p>
+            <p>🥈The 2nd holder will get 250,000 OCTIES</p>
+            <p>🥉The 3rd holder will get 100,000 OCTIES</p>
           </div>
-        
-          <div className='Lb_Liders'>
-            <p>{userCount} Users</p>
-          </div>
-          <div className='Lb_list'>
-            {leaderboard.map((user, index) => (
-                <div  key={user._id} className='Lb_Lider'>
-                    <div className='LbPhoto'>
-                      <div className="RamdomImage" 
+        </div>
 
-                        style={{
-                          backgroundColor: getRandomColor(), 
-                          borderRadius: '50%', 
-                          aspectRatio: '1', 
-                          height: '60%', 
-                          display: 'flex', 
-                          alignItems: 'center', 
-                          justifyContent: 'center', 
-                          fontSize: '2.5vh',
-                          margin:'2vh', 
-                          color: 'white'}}>
-                            
-                          {user.nickname.slice(0, 2).toUpperCase()}
-                      </div>  
-                    <div className='NameLb'>
-                      <p> {user.nickname} </p>
-                      <p id='LbColor'>{user.coins} OCTIES</p>
-                    </div>
-                </div>
-                <div className='LbPhoto' id="medal">
-                  <p>{getMedal(index)}</p>
+        <div className='Lb_inside'>
+          <div className='LbPhoto'>
+            <div style={{backgroundColor: getRandomColor(), borderRadius: '50%', width: '40px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '20px', color: 'white'}}>
+              {userRank ? `#${userRank}` : '??'}
+            </div>
+            <p> {nickname} <br/><span id='LbColor'>{userRank ? `Rank: ${userRank}` : 'Loading...'}</span></p>
+          </div>
+        </div>
+      
+        <div className='Lb_Liders'>
+          <p>Top 50 holders</p>
+        </div>
+        <div className='Lb_list'>
+          {leaderboard.map((user, index) => (
+            <div key={user._id} className='Lb_Lider'>
+              <div className='LbPhotos'>
+                <div style={{backgroundColor: getRandomColor(), borderRadius: '50%', width: '40px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '20px', color: 'white'}}>
+                  {user.nickname.slice(0, 2).toUpperCase()}
                 </div>
               </div>
-            ))}
-          </div>
+              <div className='tt'>
+                <p>{user.firstName} {user.nickname}</p>
+                <p id='LbColorr'>{user.coins} OCTIES</p>
+              </div>
+              <div className='LbPhotos' id="medal">
+                <p>{getMedal(index)}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Leaderboard;
