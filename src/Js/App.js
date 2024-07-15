d3765eabd811fff2074affea36ac143486d1ac3a
Aferist163 <aferist.game.163@gmail.com>
2024-07-15 03:05:21 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../Css/App.css';
+import './App.css';
 
 import Friends from './Friends';
 import Leaderboard from './Leaderboard';
@@ -68,93 +68,94 @@ function App() {
 
   return (
     <div className="App">
-      {app && <div className='blk'></div>}
-      <div className="info">
-        <img src={Logo} alt='Logo' />
-        <div className='Txt'>
-          <img src={Play} alt='Play' />
-          <p>Your Score</p>
-        </div>
-      </div>
-      <div className="main" onClick={(event) => {localStorage.clear();}}>
-        <img src={Octo} alt='Octo' />
-      </div>
-      <div className='MainCoin'>
-        <p>{Coin} OCTIES</p>
-      </div>
-      <div className='Menu'>
-        <div className='MenuBorder'>
-          <p id='up'>OCTIES COMMUNITY</p>
-          <p id='dp'>Home for Telegram OCs</p>
-          <div className='MenuBtn'>
-            <button>Join</button>
-            <p>+ 1000 OCTIES</p>
+      <div className="content">
+        {app && <div className='blk'></div>}
+        <div className="info">
+          <img src={Logo} alt='Logo' />
+          <div className='Txt'>
+            <img src={Play} alt='Play' />
+            <p>Your Score</p>
           </div>
         </div>
-        <div className='Reward'>
-          <p>Your Rewards</p>
+        <div className="main" onClick={(event) => {localStorage.clear();}}>
+          <img src={Octo} alt='Octo' />
         </div>
-        <div className='Tasks'>
-          <div className='TS'>
-            <div className='tsPhoto'>
-              <img src={TS1} alt='TS1' /> <p id='txt'>Account age</p>
-            </div>
-            <div className='tsPhoto'>
-              <p>+838 OCTIES</p>
-            </div>
-          </div>
-
-          <div className='TS'>
-            <div className='tsPhoto'>
-              <img src={TS2} alt='TS2' /> <p id='txt'>Telegram Premium</p>
-            </div>
-            <div className='tsPhoto'>
-              <p>+500 OCTIES</p>
+        <div className='MainCoin'>
+          <p>{Coin} OCTIES</p>
+        </div>
+        <div className='Menu'>
+          <div className='MenuBorder'>
+            <p id='up'>OCTIES COMMUNITY</p>
+            <p id='dp'>Home for Telegram OCs</p>
+            <div className='MenuBtn'>
+              <button>Join</button>
+              <p>+ 1000 OCTIES</p>
             </div>
           </div>
-
-          <div className='TS'>
-            <div className='tsPhoto'>
-              <img src={TS3} alt='TS3' /> <p id='txt'>Channel Subscription</p>
-            </div>
-            <div className='tsPhoto'>
-              <p>+1,000 OCTIES</p>
-            </div>
+          <div className='Reward'>
+            <p>Your Rewards</p>
           </div>
-
-          <div className='TS'>
-            <div className='tsPhoto'>
-              <img src={TS4} alt='TS4' /> <p id='txt'>Invites</p>
+          <div className='Tasks'>
+            <div className='TS'>
+              <div className='tsPhoto'>
+                <img src={TS1} alt='TS1' /> <p id='txt'>Account age</p>
+              </div>
+              <div className='tsPhoto'>
+                <p>+838 OCTIES</p>
+              </div>
             </div>
-            <div className='tsPhoto'>
-              <p>+125,995 OCTIES</p>
+
+            <div className='TS'>
+              <div className='tsPhoto'>
+                <img src={TS2} alt='TS2' /> <p id='txt'>Telegram Premium</p>
+              </div>
+              <div className='tsPhoto'>
+                <p>+500 OCTIES</p>
+              </div>
+            </div>
+
+            <div className='TS'>
+              <div className='tsPhoto'>
+                <img src={TS3} alt='TS3' /> <p id='txt'>Channel Subscription</p>
+              </div>
+              <div className='tsPhoto'>
+                <p>+1,000 OCTIES</p>
+              </div>
+            </div>
+
+            <div className='TS'>
+              <div className='tsPhoto'>
+                <img src={TS4} alt='TS4' /> <p id='txt'>Invites</p>
+              </div>
+              <div className='tsPhoto'>
+                <p>+125,995 OCTIES</p>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className='BTNLow'>
+          <div className='LowerBTN'>
+            <div className='BTN' onClick={handleHome}>
+              <img src={IconHome} alt='IconHome' />
+            </div>
+            <div className='BTN' onClick={handleLeaderboard}>
+              <img src={IconLeaderboard} alt='IconLeaderboard' />
+            </div>
+            <div className='BTN' onClick={handleFrends}>
+              <img src={IconFriends} alt='IconFriends' />
+            </div>
+          </div>
+        </div>
+
+        {FPage && (<First onClose={handleFirstPageClose} setCheckOpen={setCheckOpen} />)}
+
+        {CheckOpen && (<Check setCheckOpen={setCheckOpen} />)}
+
+        {isLeaderboardOpen && (<Leaderboard LeaderboardAnim={LeaderboardAnim} />)}
+
+        {isFrendsOpen && (<Friends FriendsAnim={FriendsAnim} invite={invite} />)}
       </div>
-
-      <div className='BTNLow'>
-        <div className='LowerBTN'>
-          <div className='BTN' onClick={handleHome}>
-            <img src={IconHome} alt='IconHome' />
-          </div>
-          <div className='BTN' onClick={handleLeaderboard}>
-            <img src={IconLeaderboard} alt='IconLeaderboard' />
-          </div>
-          <div className='BTN' onClick={handleFrends}>
-            <img src={IconFriends} alt='IconFriends' />
-          </div>
-        </div>
-      </div>
-
-      {FPage && (<First onClose={handleFirstPageClose} setCheckOpen={setCheckOpen} />)}
-
-      {CheckOpen && (<Check  setCheckOpen={setCheckOpen}/>)}
-
-      {isLeaderboardOpen && (<Leaderboard LeaderboardAnim={LeaderboardAnim} />)}
-
-      {isFrendsOpen && (<Friends FriendsAnim={FriendsAnim} invite={invite} />)}
-
     </div>
   );
 }
