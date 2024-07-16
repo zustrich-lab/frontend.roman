18122392b7dbe1bebced0cace5899938f6f103a0
Aferist163 <aferist.game.163@gmail.com>
2024-07-17 00:28:37 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -25,7 +25,6 @@ import Octo from '../IMG/All_Logo/Octo.png';
 import invite from '../IMG/All_Logo/Invite_png.png';
 import Join from '../IMG/All_Logo/Join.png';
 
-
 const REACT_APP_BACKEND_URL = 'https://octiesback-production.up.railway.app';
 
 function App() {
