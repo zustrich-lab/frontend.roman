fc784ddb2b01bbe370b89a355bc25fb698169c06
Назар Лимар <nazarlimar@MacBook-Pro-Nazar.local>
2024-07-16 20:45:16 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -304,7 +304,7 @@ const Tg_Channel_Open_chek = () => {
               <img src={TS4} alt='TS4' /> <p id='txt'>Invites</p>
             </div>
             <div className='tsPhoto'>
-              <p>+ 0 OCTIES</p>
+              <p>+ {referralCoins}</p>
             </div>
           </div>
         </div>
