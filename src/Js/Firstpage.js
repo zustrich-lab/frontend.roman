259d7c6b6659141251425cd763143cca0e6d6dbc
Aferist163 <aferist.game.163@gmail.com>
2024-07-17 01:13:07 +0200

    initial

--- src/Js/Firstpage.js
+++ src/Js/Firstpage.js
@@ -3,6 +3,13 @@ import '../Css/First.css';
 import gipsy from'../IMG/All_Logo/flag.gif';
 
 const First = ({ onClose, setCheckOpen }) => {
+
+  function handleClickFirstWithVibration() {
+    onClose;
+    setCheckOpen(true);
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy');
+  }
+
   return (
     <div className="First_Window">
       <div className='First_octis'>
@@ -15,7 +22,7 @@ const First = ({ onClose, setCheckOpen }) => {
         <p>You’ve been in Telegram for a while, it’s <br/> time to get rewarded!</p>
       </div>
       <div className='OrangeBtn'>
-        <div className='BtnO' onClick={(event) => {onClose(event);  setCheckOpen(true);}}>
+        <div className='BtnO' onClick={handleClickFirstWithVibration}>
           <p>Wow, let’s go!</p>
         </div>
       </div>
