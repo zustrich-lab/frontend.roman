2ef603288ebbe20d6cfb101eb728e4a0235506d0
Назар Лимар <nazarlimar@MacBook-Pro-Nazar.local>
2024-07-17 17:42:32 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -100,12 +100,12 @@ function App() {
         if (data.hasCheckedSubscription) {
           localStorage.setItem('Galka', 'true');
           localStorage.setItem('Knopka', 'false');
-          
+          setCoins(data.coins);
 
         } else {
           localStorage.setItem('Galka', 'false');
           localStorage.setItem('Knopka', 'true');
-          
+          setCoins(data.coins);
         }
 
         if (hasTelegramPremium === true){
