639850d7632a727f064683800d672fad76e1fc55
Aferist163 <aferist.game.163@gmail.com>
2024-09-09 19:25:42 +0200

    innitial

--- src/Js/P2e.js
+++ src/Js/P2e.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../Css/P2e.css';
+import soon from '../IMG/ComingSoon/Text_soon.png';
+
 
 const PlayToEarn = ({p2eAnim}) => {
 
   return (
     <div className={`P2E_Window ${p2eAnim ? 'fade-out' : ''}`}>
-      <p>Coming soon</p>
+      <img src={soon}></img>
     </div>
 
 );
