23423ec259cc168e9eea9266a22540cb05e38fb6
Aferist163 <aferist.game.163@gmail.com>
2024-09-09 19:26:54 +0200

    innitial

--- src/Js/P2e.js
+++ src/Js/P2e.js
@@ -7,7 +7,7 @@ const PlayToEarn = ({p2eAnim}) => {
 
   return (
     <div className={`P2E_Window ${p2eAnim ? 'fade-out' : ''}`}>
-      <img src={soon}></img>
+      <img src={soon} alt=''></img>
     </div>
 
 );
