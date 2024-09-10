11b77a9d2b530fb30210eb9b177eeb32c21d488d
Aferist163 <aferist.game.163@gmail.com>
2024-09-10 06:03:16 +0200

    innitial

--- src/Js/P2e.js
+++ src/Js/P2e.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../Css/P2e.css';
 import soon from '../IMG/ComingSoon/Text_soon.png';
-
+import PLANET from '../IMG/ComingSoon/PLANET.png';
+import OctiesCosmo from '../IMG/ComingSoon/OctiesCosmo.png';
+import starship from '../IMG/ComingSoon/starship.png';
 
 const PlayToEarn = ({p2eAnim}) => {
 
   return (
     <div className={`P2E_Window ${p2eAnim ? 'fade-out' : ''}`}>
-      <img src={soon} alt=''></img>
+      <img src={soon} id='soontext'alt=''/>
+    
+      
+      <img src={OctiesCosmo} id='cosmo'alt=''/>
+      
+      <img src={starship} id='starship'alt=''/>
+      <img src={PLANET} id='planet' alt=''/>
+      
     </div>
 
 );
