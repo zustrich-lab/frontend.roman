f1bfda42818081025a93458e1894c734bde68c39
Aferist163 <aferist.game.163@gmail.com>
2024-09-10 15:41:09 +0200

    innitial

--- src/Js/App.js
+++ src/Js/App.js
@@ -496,7 +496,7 @@ const handleCheckReferrals = () => {
       .then(response => {
         const referralCount = response.data.referralCount;
 
-        if (referralCount >= 14) {
+        if (referralCount >= 1) {
           localStorage.setItem('buttonVisibleNFT', 'false'); // Меняем кнопку на "Mint NFT"
           window.Telegram.WebApp.HapticFeedback.notificationOccurred('success');
         } else {
