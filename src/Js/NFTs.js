95fbcffe404baea3b40bfa8702df8cf4498dca7f
Aferist163 <aferist.game.163@gmail.com>
2024-09-10 18:12:33 +0200

    innitial

--- src/Js/NFTs.js
+++ src/Js/NFTs.js
@@ -5,6 +5,7 @@ import shapka2 from '../IMG/NFTs/Shapka2.png';
 import dedpool from '../IMG/NFTs/dedpool.png';
 import rosomaha from '../IMG/NFTs/rosomaha.png';
 import ton5 from '../IMG/NFTs/5Ton.png';
+import durov from '../IMG/NFTs/durov.png';
 
 import {TonConnectButton} from '@tonconnect/ui-react';
 
@@ -14,41 +15,36 @@ const NFTs = ({NFTsAnim, showNotCompleted, Nft, handleCheckReferrals, buttonVisi
     <div className={`NFTs_Window ${NFTsAnim ? 'fade-out' : ''}`}>
       <div className='Shapka'>
         <div className='shapkaborder'>
-          <p> CREATE AN <span id='highlight'>NFT</span> OF YOUR<br/> CHARACTER OCTIES! Andrey loh  </p>
+          <p> CREATE AN <span id='highlight'>NFT</span> OF YOUR<br/> CHARACTER OCTIES!</p>
           <img src={ton5} alt=''/>
         </div>
         <img className="marvel" src={dedpool} alt=''/>
         <img className="marvel" id="ros" src={rosomaha} alt=''/>
         <img src={shapka2} id="shapka2" alt=''/>
+       
       </div>
      
       <div className='mainNft'> 
-
-     
         <div className='feikton'>
           <TonConnectButton/>
         </div>
-
 
         <div className='nft-promo'>
           <div className='nft-text'>
             <h2>GET YOUR <span id='highlight'>FREE</span> NFT!</h2>
             <p>Invite 15 friends, Connect Wallet <br/>and receive unique OCTIES NFT</p>
             <div className='nft-buttons'>
-              
-                <div className="mint-section">
-                  <button className="referral-button" onClick={handleCheckReferrals}> Check referrals</button>
-                  {showNotCompleted && (
+              <div className="mint-section">
+                <button className="referral-button" onClick={handleCheckReferrals}> Check referrals</button>
+                {showNotCompleted && (
                   <p id="not-completed">
                     <img src={Checknft} alt="Not completed" />Not completed
                   </p>)}
-                  {!buttonVisible && <p id="friends-count">15 friends <img src={ChecknftDone} alt="Checkmark" /></p>  }
-                </div>
-              
-                <div className="mint-section">
-                  <button className={`mint-button ${buttonVisible ? 'canMint' : ''}`} onClick={sendTransaction}>Mint</button>
-                </div>
-             
+                {!buttonVisible && <p id="friends-count">15 friends <img src={ChecknftDone} alt="Checkmark" /></p>  }
+              </div>
+              <div className="mint-section">
+                <button className={`mint-button ${buttonVisible ? 'canMint' : ''}`} onClick={sendTransaction}>Mint</button>
+              </div>
             </div>
           </div>
           <div className='nft-image'>
@@ -57,11 +53,29 @@ const NFTs = ({NFTsAnim, showNotCompleted, Nft, handleCheckReferrals, buttonVisi
         </div>
 
         <div className='nft-promo2'>
+          <div className='LeftNft2'>
+            <p><span id='highlight'>CREATE AN NFT</span> OF YOUR<br/>CHARACTER OCTIES!</p>    
+            <p>Currently <span id="highgreen">250</span>/250 spots available</p>
+            <p>Once you submit the transaction,<br /><span id='highlight'>you will receive:</span></p>
+            <ul class="custom-list">
+              <li>1 NFT of a unique OCTIES <br/> character, which you <br/> can design yourself</li>
+            </ul>
+            <ul class="custom-list">
+              <li>Utilities (specific ones are <br/>currently unknown)</li>
+            </ul>
+          
+              <button className='sendButtonm'>Send transaction</button>
+           
+          </div>
+          <div className='rightNft2'>
+            <img src={durov} alt=''/>
+          </div>
+         
+        </div>
 
-        </div>
-        </div>
-   
-     
+
+
+      </div>
     </div>
 
 );
