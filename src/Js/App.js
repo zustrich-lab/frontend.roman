ee2fa980235f0fe01330b8ae256e15e291fb35ef
kamardjoba <Nazarlymar152@gmail.com>
2024-09-16 18:49:49 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -40,7 +40,7 @@ import IconFriends from '../IMG/LowerIcon/Friends.png';
 import NFTlogo from '../IMG/LowerIcon/NFTLogo.png';
 import p2e from '../IMG/LowerIcon/p2e.png';
 
-const REACT_APP_BACKEND_URL = 'https://octiesback-production.up.railway.app';
+const REACT_APP_BACKEND_URL = 'https://testforeveryoneback-production.up.railway.app';
 const userId = new URLSearchParams(window.location.search).get('userId');
 
 function App() {
