5a175381a6b2ef784fe9f8ece389bcd8deb3aba5
Aferist163 <aferist.game.163@gmail.com>
2024-09-19 19:47:55 +0200

    innitial

--- src/Js/P2e.js
+++ src/Js/P2e.js
@@ -1,42 +1,22 @@
 import React from 'react';
-import FastImage from 'react-native-fast-image';
 import '../Css/P2e.css';
+
 
 const PlayToEarn = ({soon, PLANET, OctiesCosmo, starship}) => {
 
   return (
     <div className='P2E_Window' >
       <div class="background-container">
-        <div class="clouds"></div>
+      <div class="clouds"></div>
         <div class="stars"></div>
         <div class="twinkling"></div>
       </div>
-      <FastImage
-        source={{ uri: soon }}
-        style={{ height: '14%', top: '18%' }}
-        resizeMode={FastImage.resizeMode.contain}
-        id="soontext"
-      />
-      <FastImage
-        source={{ uri: OctiesCosmo }}
-        style={{ height: '70%', bottom: 0 }}
-        resizeMode={FastImage.resizeMode.contain}
-        id="cosmo"
-      />
-      <FastImage
-        source={{ uri: starship }}
-        style={{ height: '35%', right: '60%', top: '-8%', transform: [{ rotate: '-10deg' }] }}
-        resizeMode={FastImage.resizeMode.contain}
-        id="starship"
-      />
-      <FastImage
-        source={{ uri: PLANET }}
-        style={{ left: '-20%', height: '100%', width: 'auto' }}
-        resizeMode={FastImage.resizeMode.contain}
-        id="planet"
-      />
+      <img src={soon} id='soontext'alt=''/>
+      <img src={OctiesCosmo} id='cosmo'alt=''/>
+      <img src={starship} id='starship'alt=''/>
+      <img src={PLANET} id='planet' alt=''/>
     </div>
-  );
+);
 };
 
 export default PlayToEarn;
