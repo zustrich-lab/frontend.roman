cf5e013d67afecfa55eb20e3a0125aa697c260e0
Aferist163 <aferist.game.163@gmail.com>
2024-09-20 17:31:06 +0200

    innitial

--- src/NftPages/Missed.js
+++ src/NftPages/Missed.js
@@ -4,30 +4,30 @@ import '../Css/NFTs.css';
 
 const MissedNFT = ({Nft, Checknft, isMint }) => {
 
-  return (
-    <div className='mainNftMissed'> 
-        {!isMint && (<div className='nft-promo'>
-            <div className='nft-text'>
-                <h2>GET YOUR <span id='highlight'>FREE</span> NFT!</h2>
-                <p>Invite 15 friends, Connect Wallet <br/>and receive unique OCTIES NFT</p>
-                <div className='nft-buttons'>
-                    <div className="mint-section">
-                        <button id="not-completed-missed" >
-                            <p>15 friends </p>
-                            <img src={Checknft} alt="Not completed" />
-                        </button>
-                    </div>
-                    <div className="mint-section">
-                        <button className='mint-button' id='redlight'> MISSED</button>
+    return (
+        <div className='mainNftMissed'> 
+            {!isMint && (<div className='nft-promo'>
+                <div className='nft-text'>
+                    <h2>GET YOUR <span id='highlight'>FREE</span> NFT!</h2>
+                    <p>Invite 15 friends, Connect Wallet <br/>and receive unique OCTIES NFT</p>
+                    <div className='nft-buttons'>
+                        <div className="mint-section">
+                            <button id="not-completed-missed" >
+                                <p>15 friends </p>
+                                <img src={Checknft} alt="Not completed" />
+                            </button>
+                        </div>
+                        <div className="mint-section">
+                            <button className='mint-button' id='redlight'> MISSED</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='nft-image'>
-                <img src={Nft} alt='OCTIES NFT' /> 
-            </div>
-        </div>)}
-    </div>
-  );
+                <div className='nft-image'>
+                    <img src={Nft} alt='OCTIES NFT' /> 
+                </div>
+            </div>)}
+        </div>
+    );
 };
 
 export default MissedNFT;
