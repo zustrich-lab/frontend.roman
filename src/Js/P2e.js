e3ee98bdf4209d4d0b3e57d1cb076d6c1cc8713d
Aferist163 <aferist.game.163@gmail.com>
2024-10-04 19:32:04 +0200

    innitial

--- src/Js/P2e.js
+++ src/Js/P2e.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../Css/P2e.css';
-import { P2e_Soon, P2e_OctiesCosmo,
-         P2e_Planet, P2e_starship} from "../IMG/ComingSoon";
 
-const PlayToEarn = () => {
+
+const PlayToEarn = ({soon, PLANET, OctiesCosmo, starship}) => {
 
   return (
     <div className='P2E_Window' >
@@ -12,10 +11,10 @@ const PlayToEarn = () => {
         <div class="stars"></div>
         <div class="twinkling"></div>
       </div>
-      <img src={P2e_Soon} id='soontext'alt=''/>
-      <img src={P2e_OctiesCosmo} id='cosmo'alt=''/>
-      <img src={P2e_starship} id='starship'alt=''/>
-      <img src={P2e_Planet} id='planet' alt=''/>
+      <img src={soon} id='soontext'alt=''/>
+      <img src={OctiesCosmo} id='cosmo'alt=''/>
+      <img src={starship} id='starship'alt=''/>
+      <img src={PLANET} id='planet' alt=''/>
     </div>
 );
 };
