05e4473bd7244335d5946a380546134b2f4b9fda
Aferist163 <aferist.game.163@gmail.com>
2024-10-10 18:25:04 +0200

    innitial

--- src/Js/P2e.js
+++ src/Js/P2e.js
@@ -4,10 +4,10 @@ import LoadingScreenGame from '../Loading/LoadingGame.jsx';
 
 function  PlayToEarn() {
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "buildTgGameUnity/Game.loader.js",
-    dataUrl: "buildTgGameUnity/Game.data.unityweb",
-    frameworkUrl: "buildTgGameUnity/Game.framework.js.unityweb",
-    codeUrl: "buildTgGameUnity/Game.wasm.unityweb",
+    loaderUrl: "buildTgGameUnity/Build.loader.js",
+    dataUrl: "buildTgGameUnity/Build.data.unityweb",
+    frameworkUrl: "buildTgGameUnity/Build.framework.js.unityweb",
+    codeUrl: "buildTgGameUnity/Build.wasm.unityweb",
   });
 
   return (
