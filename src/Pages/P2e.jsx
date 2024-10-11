f295645c3d3f943f5ba6f3b85e5095b1f15af21e
Aferist163 <aferist.game.163@gmail.com>
2024-10-12 00:51:20 +0200

    innitial

--- src/Pages/P2e.jsx
+++ src/Pages/P2e.jsx
@@ -1,59 +1,18 @@
-import React from "react";
-import { Unity, useUnityContext } from "react-unity-webgl";
-import LoadingScreenGame from '../Loading/LoadingGame.jsx';
+import React from 'react';
+import '../Css/P2e.css';
 
-function  PlayToEarn() {
-  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "buildTgGameUnity/Build.loader.js",
-    dataUrl: "buildTgGameUnity/Build.data.unityweb",
-    frameworkUrl: "buildTgGameUnity/Build.framework.js.unityweb",
-    codeUrl: "buildTgGameUnity/Build.wasm.unityweb",
-  });
+const PlayToEarn = () => {
 
-  return (
-    <>
-      {!isLoaded && (
-        <>  
-          <div style={{ 
-            position: "absolute",
-            top: "55vh",
-            width: `${Math.round(loadingProgression * 90)}%`,
-            height: "2vh",
-            margin: "5%",
-            borderRadius: "50vh",
-            background: '#D06C24',
-            zIndex: "1"}}
-          /> 
-          <div style={{ 
-            position: "absolute",
-            top: "55vh",
-            width: '90%',
-            margin: "5%",
-            height: "2vh",
-            borderRadius: "50vh",
-            background: '#1A1A1A',
-            zIndex: "0"}}
-          /> 
-          <p style={{ 
-            position: "absolute",
-            top: "65vh",
-            alignSelf: "center"}}>
-              {Math.round(loadingProgression * 100)}%
-          </p> 
-          <LoadingScreenGame/>
-        </>
-      )}
-      <Unity
-        unityProvider={unityProvider}
-        style={{ 
-          visibility: isLoaded ? "visible" : "hidden",
-          height:"100%",
-          width:"100%",
-          justifySelf: "center",
-          alignSelf: "center"}}
-      />
-    </>
-  );
+    return (
+        <div className='P2E_Window'>
+            <iframe 
+                src="https://octiesdev.github.io/test_game_v8/" 
+                width="100%" 
+                height="100%" 
+                style={{ border: 'none' }}
+            />
+        </div>
+    );
 }
 
 export default PlayToEarn;
