a4f2cf46adafdcd1172c1a0e85ddb6f1da8d1bb1
kamardjoba <Nazarlymar152@gmail.com>
2024-10-11 21:00:29 +0200

    initial

--- src/Js/Home.js
+++ src/Js/Home.js
@@ -93,7 +93,7 @@ function Home({Galo4ka, Knopka, Galo4kaX, KnopkaX,  GalkaAnyTap, KnopkaAnyTap, K
 const showAd = async () => {
   try {
       // const response = await fetch(`https://testforeveryoneback-production.up.railway.app/get-ads-watched?telegramId=${userId}`);
-      const response = await axios.post(`https://testforeveryoneback-production.up.railway.app/get-ads-watched`, { userId });
+      const response = await axios.get(`https://testforeveryoneback-production.up.railway.app/get-ads-watched`, { userId });
       const data = await response.json();
       
       if (data.success) {
