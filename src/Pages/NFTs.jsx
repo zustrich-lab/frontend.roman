a42abe3920cdd99c1adba41fb491d39805de03a9
kamardjoba <Nazarlymar152@gmail.com>
2024-10-12 19:13:53 +0200

    initial

--- src/Pages/NFTs.jsx
+++ src/Pages/NFTs.jsx
@@ -151,7 +151,7 @@ const handleComplatedNFT = () => {
       {alert && <AlertNft rosomaha={rosomaha} setalert={setalert}/>}
       <div className='Shapka'>
         <div className='shapkaborder'>
-          <p> CREATE AN <span id='highlight'>NFT</span> OF YOUR<br/> CHARACTER OCTIES!</p>
+          <p> GET YOUR <span id='highlight'>REFERRAL</span> NFT! <br/> Invite 15 friends & Connect Wallet <span id='highlight'>v.2</span></p>
           {/* <img src={ton5} onClick={sendTransaction1} alt=''/> */}
         </div>
         <img className="marvel" src={dedpool} alt=''/>
