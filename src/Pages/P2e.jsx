31539d343b80acb5b486ea1d9515c91042959fdb
kamardjoba <Nazarlymar152@gmail.com>
2024-10-17 16:57:53 +0200

    initial

--- src/Pages/P2e.jsx
+++ src/Pages/P2e.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import '../Css/P2e.css';
+// import React from 'react';
+// import '../Css/P2e.css';
+
+// const PlayToEarn = () => {
+
+//     return (
+//         <div className='P2E_Window'>
+//             <iframe 
+//                 src="https://octiesdev.github.io/test_game_v8/" 
+//                 width="100%" 
+//                 height="100%" 
+//                 style={{ border: 'none' }}
+//             />
+//         </div>
+//     );
+// }
+
+// export default PlayToEarn;
+
+import React, { useEffect, useState } from 'react';
 
 const PlayToEarn = () => {
+  const [userId, setUserId] = useState(null);
 
-    return (
-        <div className='P2E_Window'>
-            <iframe 
-                src="https://octiesdev.github.io/test_game_v8/" 
-                width="100%" 
-                height="100%" 
-                style={{ border: 'none' }}
-            />
-        </div>
-    );
-}
+  useEffect(() => {
+    // Получаем userId из параметров или контекста приложения
+    const urlParams = new URLSearchParams(window.location.search);
+    const userIdFromTelegram = urlParams.get('userId');
+    setUserId(userIdFromTelegram);
+  }, []);
+
+  const gameUrl = `https://octiesdev.github.io?userId=${userId}`;
+
+  return (
+    <div>
+      {userId ? (
+        <iframe src={gameUrl} width="800" height="600" title="Game" />
+      ) : (
+        <p>Loading game...</p>
+      )}
+    </div>
+  );
+};
 
 export default PlayToEarn;
